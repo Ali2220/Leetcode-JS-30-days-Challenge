403fa78848f24d27468f7cd4c1cb6078048d6a74
Ali2220 <alisarwar0277@gmail.com>
2025-12-04 22:59:22 +0500

    2 more question done of function transformations

---
+++ Function Transformation/Q3.js
@@ -0,0 +1,24 @@
+// Given a function fn, return a new function that is identical to the original function except that it ensures fn is called at most once.
+
+// The first time the returned function is called, it should return the same result as fn.
+// Every subsequent time it is called, it should return undefined.
+
+let once = function (fn) {
+  let called = false;
+  let result;
+
+  return function (...args) {
+    if (!called) {
+      called = true;
+      result = fn(...args);
+      return result;
+    } else {
+      return undefined;
+    }
+  };
+};
+
+const add = (a, b) => a + b;
+const onceAdd = once(add);
+console.log(onceAdd(1, 2));
+console.log(onceAdd(1, 2));
---
+++ Function Transformation/Q4.js
@@ -0,0 +1,29 @@
+// A memoized function is a function that will never be called twice with the same inputs. Instead it will return a cached value.
+
+// You can assume an input function: sum
+
+// sum accepts two integers a and b and returns a + b. Assume that if a value has already been cached for the arguments (b, a) where a != b, it cannot be used for the arguments (a, b). For example, if the arguments are (3, 2) and (2, 3), two separate calls should be made
+
+
+let memoize = function(fn){
+    const cache = {}
+
+    return function(...args){
+        const key = JSON.stringify(args)
+
+        if(key in cache){
+            return cache[key]    // return cached result
+        }
+
+        const result = fn(...args)   // compute result
+        cache[key] = result    // save to cache
+        return result
+        
+    }
+}
+
+const sum = (a, b) => a + b
+const memoizedSum = memoize(sum)
+console.log(memoizedSum(1, 2));
+console.log(memoizedSum(1, 2));
+console.log(memoizedSum(2, 2));
