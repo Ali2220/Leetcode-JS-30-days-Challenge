7982e145c317fa3b9b43aaffb9db288eefe54a8d
Ali2220 <alisarwar0277@gmail.com>
2025-12-04 23:00:02 +0500

    nothing

--- Function Transformation/Q4.js
+++ Function Transformation/Q4.js
@@ -4,26 +4,24 @@
 
 // sum accepts two integers a and b and returns a + b. Assume that if a value has already been cached for the arguments (b, a) where a != b, it cannot be used for the arguments (a, b). For example, if the arguments are (3, 2) and (2, 3), two separate calls should be made
 
+let memoize = function (fn) {
+  const cache = {};
 
-let memoize = function(fn){
-    const cache = {}
+  return function (...args) {
+    const key = JSON.stringify(args);
 
-    return function(...args){
-        const key = JSON.stringify(args)
-
-        if(key in cache){
-            return cache[key]    // return cached result
-        }
-
-        const result = fn(...args)   // compute result
-        cache[key] = result    // save to cache
-        return result
-        
+    if (key in cache) {
+      return cache[key]; // return cached result
     }
-}
 
-const sum = (a, b) => a + b
-const memoizedSum = memoize(sum)
+    const result = fn(...args); // compute result
+    cache[key] = result; // save to cache
+    return result;
+  };
+};
+
+const sum = (a, b) => a + b;
+const memoizedSum = memoize(sum);
 console.log(memoizedSum(1, 2));
 console.log(memoizedSum(1, 2));
 console.log(memoizedSum(2, 2));
